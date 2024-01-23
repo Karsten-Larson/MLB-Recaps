7f28deb7fc1fc985ef18e4cdca7008e6b1130758
MrRedwing <karsten.larson.1@gmail.com>
2024-01-23 15:39:36 -0600

    Changed generate month to new DateRange object

--- src/date/date_generator.py
+++ src/date/date_generator.py
@@ -1,6 +1,8 @@
-from typing import Type, List
-from datetime import datetime
 from .date import Date
+from .date_range import DateRange
+
+from typing import Type
+from datetime import datetime
 from copy import copy
 
 class DateGenerator():
@@ -27,15 +29,20 @@ class DateGenerator():
         return obj
 
     @classmethod
-    def month(cls, month: int, year: int) -> List[Type["Date"]]:
-        firstDate = Date(month, 1, year)
-        dates = []
+    def month(cls, month: int, year: int) -> Type["DateRange"]:
+        start_dt = Date(month, 1, year)
 
-        while firstDate.date.month == month:
-            dates.append(copy(firstDate))
-            firstDate.next()
+        # set the end date to the end of the month
+        end_dt = copy(start_dt)
+        end_dt.next(28)
 
-        return dates
+        # check if it is still in the month then go one back
+        while end_dt.date.month == month:
+            end_dt.next()
+
+        end_dt.prev()
+
+        return DateRange(start_dt, end_dt)
 
 if __name__ == "__main__":
     print("Ran")
